1455b8b23ee5892fb1dbc421986d4c7e465e977f
Teemu Järvenpää <teemu.jarvenpaa@hamk.fi>
2020-12-10 10:18:16 +0200

    Update datareader.py
    Add light and colour readings

--- datareader.py
+++ datareader.py
@@ -16,12 +16,20 @@ def Temp_Data_Handler(jsonData):
         json_Dict = json.loads(jsonData)
         SensorID = json_Dict['Sensor_ID']
         Temperature = json_Dict['Temperature']
+        r, g, b = json_Dict['Colour']
         #Push into statsd
         temp_statsd = StatsClient(host='172.18.138.55', port=8125, prefix='Temperature')
         temp_statsd.gauge(SensorID, Temperature)
 
         print("Inserted Temperature Data into Database.")
         print("")
+        light_statsd = StatsClient(host='localhost', port=8125, prefix='Light')
+        light_statsd.gauge(SensorID + " " + str("r"), r)
+        light_statsd.gauge(SensorID + " " + str("g"), g)
+        light_statsd.gauge(SensorID + " " + str("b"), b)
+        print("Inserted Light Data into Database.")
+        print("")
+
 
 # Function to save Pressure to statsd
 def Pressure_Data_Handler(jsonData):
@@ -29,6 +37,7 @@ def Pressure_Data_Handler(jsonData):
         json_Dict = json.loads(jsonData)
         SensorID = json_Dict['Sensor_ID']
         Pressure = json_Dict['Pressure']
+        Light = json_Dict['Light']
 
 
         #Push into statsd
@@ -36,6 +45,11 @@ def Pressure_Data_Handler(jsonData):
         pres_statsd.gauge(SensorID, Pressure)
         print("Inserted Pressure Data into Database.")
         print("")
+        light_statsd = StatsClient(host='localhost', port=8125, prefix='Light')
+        light_statsd.gauge(SensorID, Light)
+        print("Inserted Light Data into Database.")
+        print("")
+
 #===============================================================
 #kanta function valinta topikin perusteella
 def sensor_Data_Handler(Topic, jsonData):
